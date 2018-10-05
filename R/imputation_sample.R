8d89d4e20add41541c328163697bd2aff0b23591
easbi <14.8096@stis.ac.id>
2018-10-06 05:57:54 +0700

    Update imputation_sample.R
    Mengizinkan penggunaan nilai flag yang sama

--- R/imputation_sample.R
+++ R/imputation_sample.R
@@ -5,21 +5,21 @@
 #' @param weight_aggregate Besaran agregat weight dari sampel terpilih yang diinginkan.
 #' @param weight_col Kolom yang digunakan sebagai weight dalam pemilihan sampel apabila tersedia.
 #' @param iter Jumlah iterasi pengacakan dan pengambilan sampel yang diinginkan (semakin tinggi maka weight akan semakin sesuai).
-#' @param sample_flag Identitas dari sampel yang dihasilkan.
+#' @param sample_flag Identitas dari sampel yang dihasilkan. Untuk kondisi default sample_flag bernilai (integer) 1
 #' @return Data yang telah diberi flag untuk sampel terpilih yang selanjutnya dapat diubah atributnya menggunakan fungsi \code{\link{mutate_sample}}.
 #' @examples
 #' # Membuat filter berbeda
 #' filter_1 = create_filter(NAMA_PROV == "ACEH", KLASIFIKASI == 1)
 #' filter_2 = create_filter(NAMA_PROV == "RIAU" | NAMA_PROV == "SUMATERA BARAT", KLASIFIKASI == 2)
-#' imputation_sample(x = sakernas_dummy, filters = filter_1, weight_aggregate = 10000, weight_col = Weight_R, sample_flag = "aceh_1")
-#' imputation_sample(x = sakernas_dummy, filters = filter_2, weight_aggregate = 5000, weight_col = Weight_R, sample_flag = "riau_sumbar_2")
+#' imputation_sample(x = sakernas_dummy, filters = filter_1, weight_aggregate = 10000, weight_col = Weight_R)
+#' imputation_sample(x = sakernas_dummy, filters = filter_2, weight_aggregate = 5000, weight_col = Weight_R)
 #'
 #' # Membandingkan hasil sampel dengan jumlah iterasi berbeda
 #' my_filter = create_filter(NAMA_PROV == "SUMATERA BARAT", KLASIFIKASI == 2)
 #' imputation_sample(x = sakernas_dummy, filters = my_filter, weight_aggregate = 73955, weight_col = Weight_R, iter = 1)
 #' imputation_sample(x = sakernas_dummy, filters = my_filter, weight_aggregate = 73955, weight_col = Weight_R, iter = 100)
 #' @export
-imputation_sample <- function(x, filters, weight_aggregate, weight_col, iter = 1, sample_flag = "1") {
+imputation_sample <- function(x, filters, weight_aggregate, weight_col, iter = 1, sample_flag = 1) {
   weight_col <- dplyr::enquo(weight_col)
 
   if (!"temp_id" %in% colnames(x)) {
@@ -28,13 +28,8 @@ imputation_sample <- function(x, filters, weight_aggregate, weight_col, iter = 1
 
   if (!"flag" %in% colnames(x)) {
     x$flag <- NA # Create default column for flagging
-  } else {
-    all_flags = unique(x$flag)
-    if (sample_flag %in% all_flags) {
-      stop('Flag sudah ada untuk sampel lain, silakan buat flag baru dengan mengatur parameter flag (contoh: sample_flag = "aceh_desa_perempuan"')
-    }
-  }
-
+  } 
+  
   x_filtered <- x %>%
     dplyr::filter(!!!filters)
 
